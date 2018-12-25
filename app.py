2ed85395e1eb505a03012696a5f368ca454e3f6a
Stephen Young <wy1309374767@gmail.com>
2018-12-25 16:18:48 +0800

    add ip get

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import (
     Flask,
     render_template,
     request,
+    jsonify,
 )
-import random
 
 app = Flask(__name__)
+
 
 from mongo import find_, insert_, insert_new, findOrigin
 from regular import *
@@ -22,7 +23,6 @@ def hello_world(now):
     return render_template('index.html', before = before, after = after, percent_before = percent_before, percent_after = percent_after, name = name, id = id)
 
 
-
 @app.route('/figure', methods=['GET', 'POST'])
 def figure():
     li = int(request.args.get("id"))
@@ -37,32 +37,28 @@ def figure():
         print("before", before)
         print("after", after)
         if ret(before) and ret(after):
-            # print(group(before+','+after))
             a = final(before)
             percent_a = percent(a)
             b = final(after)
             percent_b = percent(b)
-            # print(b)
             insert_(id, "before",a)
             insert_(id, "after", b)
             insert_(id, "percent_before", percent_a)
             insert_(id, "percent_after", percent_b)
             print("before: {}; after: {}".format(a, b))
             print('*'*30)
-            # return render_template('re.txt', seq = find())
             return render_template('re.txt')
         else:
             print("data form is wrong")
             return "invalid data form"
     elif request.method == "GET":
-        # id = 17017
         before = find_(id, "before")
-        # print("before", before)
         after = find_(id, "after")
         percent_before = find_(id, "percent_before")
         percent_after = find_(id, "percent_after")
         name = find_(id, "index")
         return render_template("test.txt", before = before, after = after, percent_before = percent_before, percent_after = percent_after, name = name)
+
 
 @app.route("/co2/data", methods=['GET', 'POST'])
 def co2_data():
@@ -78,6 +74,7 @@ def co2(id):
     data = after[2]
     return render_template("co2.html", id = id, data = data)
 
+
 @app.route("/index", methods=['GET', 'POST'])
 def index():
     id = 2
@@ -88,14 +85,13 @@ def index():
     return (json.dumps(list))
 
 
-
-
-
-
-
 @app.route('/tool')
 def tool():
     return render_template('form.html')
+
+@app.route('/myip', methods=['GET'])
+def get_my_ip():
+    return jsonify({'ip': request.remote_addr}), 200
 
 
 if __name__ == '__main__':
